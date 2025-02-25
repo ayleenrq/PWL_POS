1a7625608e47a103a32a73a125d17f37d8b585c9
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-02-25 21:44:46 +0700

    Jobsheet 3_Praktikum 6: Langkah 10_Update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Tambah data user dengan Eloquent Model
+        /// Tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'), // Menggunakan Hash untuk menyimpan password
-            'level_id' => 3
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::insert($data); // Tambahkan data ke tabel m_user
+
+        // Update data user dengan username 'customer-1'
+        UserModel::where('username', 'customer-1')->update($data);
 
         // Coba akses model UserModel
         $user = UserModel::all(); // Ambil semua data dari tabel m_user
-        return view('user', ['user' => $user]); // Kirim data ke view
+        return view('user', ['user' => $data]); // Kirim data ke view
     }
 }
