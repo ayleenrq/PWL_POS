c9472c607133b64d3528fccccae0891bbab80030
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-02-25 19:35:57 +0700

    Jobsheet 3_Praktikum 5: Langkah 2_Update Route KategoriController [FIX]

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
