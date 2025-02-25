70ccca4f44a8d8a04ef6a440ac88c7576d1ab068
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-02-25 21:44:04 +0700

    Jobsheet 3_Praktikum 6: Langkah 8_Update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
-        return view('user', ['user' => $user]);
+        // Tambah data user dengan Eloquent Model
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'), // Menggunakan Hash untuk menyimpan password
+            'level_id' => 3
+        ];
+        UserModel::insert($data); // Tambahkan data ke tabel m_user
+
+        // Coba akses model UserModel
+        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        return view('user', ['user' => $user]); // Kirim data ke view
     }
 }
