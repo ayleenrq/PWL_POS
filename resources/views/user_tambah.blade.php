26d041196c01cd80675cb971cfae24b61ab149fc
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-03-04 15:38:27 +0700

    Jobsheet 4_Praktikum 2.6: Langkah 4_Create (user_tambah.blade)

---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Form Tambah Data User</h1>
+        <form method="post" action="/user/tambah_simpan">
+            
+            {{ csrf_field() }}
+
+            <label for="username">Username</label>
+            <input type="text" name="username" placeholder="Masukkan Username">
+            <br>
+            <label for="nama">Nama</label>
+            <input type="text" name="nama" placeholder="Masukkan Nama">
+            <br>
+            <label for="password">Password</label>
+            <input type="password" name="password"zz placeholder="Masukkan Password">
+            <br>
+            <label for="level_id">Level ID</label>
+            <input type="number" name="level_id" placeholder="Masukkan ID Level">
+            <br><br>
+            <input type="submit" class="btn btn-success" value="Simpan">
+            
+        </form>
+    </body>
+</html>
