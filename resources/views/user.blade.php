5e13156c409a360617893ddd2be5350216e86806
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-03-04 22:25:02 +0700

    update user.blade

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -21,7 +21,7 @@
                     <td>{{ $d->nama }}</td>
                     <td>{{ $d->level_id }}</td>
                     <td>
-                        <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
+                        <a href="/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a>
                     </td>
                 </tr>
             @endforeach
