ea7d0ee513b79abe91fdbcc6304a9fbf775502d6
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-03-10 15:25:41 +0700

    Jobsheet 5_Praktikum 3: Langkah 9_Add UserController create()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Yajra\DataTables\Facades\DataTables;
+use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -44,6 +45,29 @@ class UserController extends Controller
             }) 
             ->rawColumns(['aksi']) // Memberitahu bahwa kolom aksi berisi HTML 
             ->make(true); 
+    }
+
+    // Menampilkan halaman form tambah user
+    public function create()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Tambah User',
+            'list' => ['Home', 'User', 'Tambah']
+        ];
+
+        $page = (object) [
+            'title' => 'Tambah user baru'
+        ];
+
+        $level = LevelModel::all(); // Ambil data level untuk ditampilkan di form
+        $activeMenu = 'user'; // Set menu yang sedang aktif
+
+        return view('user.create', [
+            'breadcrumb' => $breadcrumb, 
+            'page' => $page, 
+            'level' => $level, 
+            'activeMenu' => $activeMenu
+        ]);
     }
 
 }
