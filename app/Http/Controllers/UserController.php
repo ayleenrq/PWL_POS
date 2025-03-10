41c3a376a56ed33207636ca299f46cfd9e588926
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-03-10 14:51:09 +0700

    Jobsheet 5_Praktikum 3: Langkah 7_Add UserController list()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,5 +25,25 @@ class UserController extends Controller
 
         return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
+    
+    // Ambil data user dalam bentuk JSON untuk DataTables 
+    public function list(Request $request) 
+    { 
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level'); 
+
+        return DataTables::of($users) 
+            // Menambahkan kolom index / nomor urut (default nama kolom: DT_RowIndex) 
+            ->addIndexColumn()  
+            ->addColumn('aksi', function ($user) {  // Menambahkan kolom aksi 
+                $btn  = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> '; 
+                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> '; 
+                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">' 
+                        . csrf_field() . method_field('DELETE') .  
+                        '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';      
+                return $btn; 
+            }) 
+            ->rawColumns(['aksi']) // Memberitahu bahwa kolom aksi berisi HTML 
+            ->make(true); 
+    }
 
 }
