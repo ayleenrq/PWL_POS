67a5a58c812eafc4149efa8567efd264d9522d2b
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-03-12 17:05:52 +0700

    Jobsheet 5_Tugas: Create Table m_barang

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,4 +63,15 @@ Route::group(['prefix' => 'supplier'], function () {
     Route::get('/{id}/edit', [SupplierController::class, 'edit']);  // menampilkan halaman form edit supplier
     Route::put('/{id}', [SupplierController::class, 'update']);     // menyimpan perubahan data supplier
     Route::delete('/{id}', [SupplierController::class, 'destroy']); // menghapus data supplier
+});
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);          // menampilkan halaman awal supplier
+    Route::post('/list', [BarangController::class, 'list']);      // menampilkan data supplier dalam bentuk json untuk datatables
+    Route::get('/create', [BarangController::class, 'create']);   // menampilkan halaman form tambah supplier
+    Route::post('/', [BarangController::class, 'store']);         // menyimpan data supplier baru
+    Route::get('/{id}', [BarangController::class, 'show']);       // menampilkan detail supplier
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);  // menampilkan halaman form edit supplier
+    Route::put('/{id}', [BarangController::class, 'update']);     // menyimpan perubahan data supplier
+    Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data supplier
 });
