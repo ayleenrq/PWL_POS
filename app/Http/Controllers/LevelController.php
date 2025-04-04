ae4aa52ca9568782d935f7692c1355c21e518df7
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-04-04 21:21:54 +0700

    Jobsheet 8_Tugas 3:  Implementasi Export PDF (Menu Level User)

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\File;
 use PhpOffice\PhpSpreadsheet\IOFactory; 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class LevelController extends Controller
 {
@@ -429,4 +430,19 @@ class LevelController extends Controller
         $writer->save('php://output');
         exit;
     } // end function export_excel
+
+    public function export_pdf()
+    {
+        $level = LevelModel::select('level_id', 'level_kode', 'level_nama')
+            ->orderBy('level_id')
+            ->get();
+
+        // use Barryvdh\DomPDF\Facade\Pdf;
+        $pdf = Pdf::loadView('level.export_pdf', ['level' => $level]);
+        $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url
+        $pdf->render();
+
+        return $pdf->stream('Data Level ' . date('Y-m-d H:i:s') . '.pdf');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,7 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
             Route::get('/import', [LevelController::class, 'import']); // menampilkan halaman import level
             Route::post('/import_ajax', [LevelController::class, 'import_ajax']); // menyimpan data level baru
             Route::get('/export_excel', [LevelController::class, 'export_excel']); // menampilkan halaman export level
+            Route::get('/export_pdf', [LevelController::class, 'export_pdf']); // export pdf
         });
     });
 
