5fb27c0214e652347bfe80b33e59353cca7243d0
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-05-01 00:41:49 +0700

    Jobsheet 10_Praktikum 1: Langkah 9

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\UserModel;
 
 class RegisterController extends Controller
 {
-    //
+    public function _invoke(Request $request){
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'nama' => 'required',
+            'password' => 'required|min:5|confirmed',
+            'level_id' => 'required',
+        ]);
+        
+        //if validations fails
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create user
+        $user = UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id,
+        ]);
+
+        //return response JSON user is created
+        if($user){
+            return response()->json([
+                'success' => true,
+                'user' => $user
+            ], 201);
+        }
+
+        //return response JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
 }
