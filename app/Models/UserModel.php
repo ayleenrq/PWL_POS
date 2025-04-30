1ced2ccf5857c8fd2ff1182cda6e6df356cfaca8
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-05-01 00:31:37 +0700

    Jobsheet 10_Praktikum 1: Langkah 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,6 +11,14 @@ class UserModel extends Authenticatable
 {
     use HasFactory;
 
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at', 'profile_picture'];
